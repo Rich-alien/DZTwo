f15d6772dc34e0f075e5ff08ca4c748e2be913f6
rich-alien <Richalien@yandex.ru>
2020-11-02 05:38:07 +0400

    bag

--- 2.3. Основы Jquery/state.js
+++ 2.3. Основы Jquery/state.js
@@ -5,82 +5,92 @@ let addToCart = (obj, count, fullPrice) => {
 
 }
 
+// function show() {
+    $.getJSON('http://localhost:3000/items', function (data) {
+        let product = data[0],
+            itemsShow = [],
+            countPrice = 1,
+            fullPrice = countPrice * data[0].priceForOne;
+        data[0].allPrice = fullPrice;
+        console.log( data[0].allPrice)
+        $.each(data, function (key, val) {
 
-$.getJSON('http://localhost:3000/items', function (data) {
-    let product = data[0],
-        itemsShow = [],
-        countPrice = 1,
-        fullPrice = countPrice * data[0].priceForOne;
-    $.each(data, function (key, val) {
+            itemsShow.push(
+                '<div class="main_container">' +
+                '<div class="product-img">' +
 
-        itemsShow.push(
-            '<div class="main_container">' +
-            '<div class="product-img">' +
+                '</div>' +
+                '<div class="product">' +
+                '<div class="product-name">' +
+                val.name +
+                '</div>' +
+                '' +
+                '<div class="product-country">' +
+                '<p class="product-description__text">' +
+                "Country:  " +
 
-            '</div>' +
-            '<div class="product">' +
-            '<div class="product-name">' +
-            val.name +
-            '</div>' +
-            '' +
-            '<div class="product-country">' +
-            '<p class="product-description__text">' +
-            "Country:  " +
+                '</p>' +
+                '<p class="product-description__text">' +
+                val.country +
+                '</p>' +
+                '</div>' +
 
-            '</p>' +
-            '<p class="product-description__text">' +
-            val.country +
-            '</p>' +
-            '</div>' +
+                '<div>' +
+                val.year_of_issue +
+                '</div>' +
 
-            '<div>' +
-            val.year_of_issue +
-            '</div>' +
+                '<div>' +
+                '<p class="product-description__text">' +
+                val.blade +
+                '</p>' +
+                '</div>' +
 
-            '<div>' +
-            '<p class="product-description__text">' +
-            val.blade +
-            '</p>' +
-            '</div>' +
+                '<div class="product-description">' +
+                '<p class="product-description__text">' +
+                val.description +
+                '</p>' +
+                '</div>' +
 
-            '<div class="product-description">' +
-            '<p class="product-description__text">' +
-            val.description +
-            '</p>' +
-            '</div>' +
+                '<div>' +
+                '<p class="product-description__text">' +
+                val.priceForOne +
+                '</p>' +
+                '</div>' +
+                '<button class="btnUp">' +
 
-            '<div>' +
-            '<p class="product-description__text">' +
-            val.priceForOne +
-            '</p>' +
-            '</div>' +
-            '<button class="btnUp">' +
+                "+1" +
 
-            "+1" +
+                '</button>' +
+                '<p class="product-description__text">' +
+                 val.allPrice+
+                    '</p>' +
+                '</div>' +
+                '</div>'
+            );
+        });
+        // вывод эллемента , который будет выбран в Store, пока что напрямую
+        let itemShow = [itemsShow[0]];
+        // $('.btnUp').on('click',function (){
+        //     countPrice++;
+        //     console.log(countPrice);
+        // });
+        //
+        // setInterval(showCount, 20);
 
-            '</button>' +
-            '<p class="product-description__text">' +
-            countPrice +
-            '</p>' +
-            '</div>' +
-            '</div>'
-        );
+        function zet() {
+            countPrice++;
+        }
+
+        $('.btnUp').click(zet)
+
+        $('<div>', {
+            'class': 'main',
+            html: itemShow.join('')
+        }).appendTo('.container');
+        addToCart(product, countPrice, fullPrice)
+
     });
-    // вывод эллемента , который будет выбран в Store, пока что напрямую
-    let itemShow = [itemsShow[0]];
-    // $('.btnUp').on('click',function (){
-    //     countPrice++;
-    //     console.log(countPrice);
-    // });
-    $('.btnUp').click(function () {
-        // countPrice++;
-        // console.log(countPrice);
-        $(this).hide();
-    })
-    $('<div>', {
-        'class': 'main',
-        html: itemShow.join('')
-    }).appendTo('.container');
-    addToCart(product, countPrice, fullPrice)
-
-});
+    // $(document).ready(function () {
+    //     show();
+    //     setInterval('show()', 1000);
+    // })
