2f4bca9b1e451a5f5c8c9b7e6f4668d0d0969929
rich-alien <Richalien@yandex.ru>
2020-11-03 06:00:27 +0400

    допилин вывод корзины

--- 2.3. Основы Jquery/state.js
+++ 2.3. Основы Jquery/state.js
@@ -1,29 +1,26 @@
 let requestURL = 'http://localhost:3000/items';
-let URLCart = 'http://localhost:3000/cart';
 let request = new XMLHttpRequest();
 let counter = 1;
 let cart = [];
+let click=0
 let chooseItem = 0; // id выбранного товара в каталоге
 let btnPlus = document.getElementById("plus"),
     btnCart = document.querySelector(".header-cart")
-    btnHideCart = document.querySelector(".header-popUp")
-    btnMinus = document.getElementById("minus"),
+btnHideCart = document.querySelector(".btn-popUp")
+btnMinus = document.getElementById("minus"),
     btnAdd = document.querySelector(".product-add");
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-let CountCart = 0;
 request.onload = function () {
     let items = request.response;
     createObj(items, chooseItem);
 
-     var cart = dropInCart(items[chooseItem]);
-
+    var cart = dropInCart(items[chooseItem]); // пришлось , чтобы выаодить кол-во эллементов в корзине
 
     updateCounter(); // при передаче аргумента item в функцию происходит bug
 
 }
-
 
 
 function throwMyEvent() {
@@ -41,11 +38,46 @@ function updateCounter() {
         if (counter > 1)
             counter = counter - 1;
     };
-    btnCart.onclick = function (){
+    //arr.splice(0,arr.length); для очистки массива , точнее удалениея, но из за моих костылей ,он ужасно получиться
+    btnHideCart.onclick = function () {
 
+        document.querySelector(".b-popup").style.display = "none";
+        // let element = document.querySelector(".popup-container");
+        // document.body.removeChild(element); // не хочет работать
+
+    }
+    btnCart.onclick = function () {
+        document.querySelector(".b-popup").style.display = "block";
+        //временное решение
+        if(click<1) {
+            for (let i = 0; i < cart.length; i++) {
+                let element = document.querySelector(".popup-container");
+                let id = document.createElement('div');
+                id.classList.add("cart-id")
+                id.innerHTML = i;
+                element.appendChild(id);
+                let name = document.createElement('div');
+                name.classList.add("cart-name")
+                name.innerHTML = `Name: ${cart[i].name}`;
+                element.appendChild(name);
+                let count = document.createElement('div');
+                count.classList.add("cart-count")
+                count.innerHTML = `Count: ${cart[i].count}`;
+                element.appendChild(count);
+                let price = document.createElement('div');
+                price.classList.add("cart-price");
+                price.innerHTML = `Price: ${cart[i].priceForOne * cart[i].count}`;
+                element.appendChild(price);
+            }
+        }
+        click++
     }
     document.querySelector('.header-cart__count').innerText = cart.length;
     document.querySelector('.count-product').innerText = counter;
+
+
+
+
 }
 
 function dropInCart(obj) {
@@ -57,9 +89,9 @@ function dropInCart(obj) {
             blade: obj.blade,
             description: obj.description,
             priceForOne: obj.priceForOne,
-            counter: counter,
+            count: counter,
         })
-            return cart;
+        return cart;
     }
 }
 
@@ -72,6 +104,7 @@ let createObj = (obj, id) => {
     //вывод информации о товаре из db
     // тут можно и через цикл , для начала создать массив их свойств и крутануть...
     document.querySelector('.product-name').innerText = obj[id].name;
+
     document.querySelector('.product-country').innerText = obj[id].country;
     document.querySelector('.product-year').innerText = obj[id].year_of_issue;
     document.querySelector('.product-blade').innerText = obj[id].blade;
@@ -81,8 +114,7 @@ let createObj = (obj, id) => {
 
 
 // хмм, времени мало , так что я запушу в массив , вместо POST в db, протите мою лень ... и да , я никогда не делал POST///
-function PopUpShow(){
 
-}
+
 document.body.addEventListener('myNewEvent', updateCounter)
 setInterval(throwMyEvent, 200)
