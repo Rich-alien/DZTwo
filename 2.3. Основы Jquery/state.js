848fed406339d3e644a5d4f3a42dbefd6ad417d4
rich-alien <Richalien@yandex.ru>
2020-11-03 04:55:01 +0400

    вывод кол-во эллементов и начало создание попапа

--- 2.3. Основы Jquery/state.js
+++ 2.3. Основы Jquery/state.js
@@ -1,26 +1,38 @@
 let requestURL = 'http://localhost:3000/items';
+let URLCart = 'http://localhost:3000/cart';
 let request = new XMLHttpRequest();
 let counter = 1;
+let cart = [];
 let chooseItem = 0; // id выбранного товара в каталоге
 let btnPlus = document.getElementById("plus"),
+    btnCart = document.querySelector(".header-cart")
+    btnHideCart = document.querySelector(".header-popUp")
     btnMinus = document.getElementById("minus"),
-    IMG = document.querySelector(".product-img"),
     btnAdd = document.querySelector(".product-add");
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-
+let CountCart = 0;
 request.onload = function () {
     let items = request.response;
     createObj(items, chooseItem);
+
+     var cart = dropInCart(items[chooseItem]);
+
+
     updateCounter(); // при передаче аргумента item в функцию происходит bug
+
 }
 
-function throwMyEvent(){
-    document.querySelector('.container').dispatchEvent(new Event('myNewEvent', {bubbles:true
+
+
+function throwMyEvent() {
+    document.querySelector('.container').dispatchEvent(new Event('myNewEvent', {
+        bubbles: true
     }));
 }
-function updateCounter(){
+
+function updateCounter() {
     btnPlus.onclick = function () {
         if (counter < 10)
             counter++;
@@ -29,10 +41,26 @@ function updateCounter(){
         if (counter > 1)
             counter = counter - 1;
     };
+    btnCart.onclick = function (){
+
+    }
+    document.querySelector('.header-cart__count').innerText = cart.length;
     document.querySelector('.count-product').innerText = counter;
 }
-function addInCart(){
 
+function dropInCart(obj) {
+    btnAdd.onclick = function () {
+        cart.push({
+            name: obj.name,
+            country: obj.country,
+            year_of_issue: obj.year_of_issue,
+            blade: obj.blade,
+            description: obj.description,
+            priceForOne: obj.priceForOne,
+            counter: counter,
+        })
+            return cart;
+    }
 }
 
 let createObj = (obj, id) => {
@@ -42,16 +70,19 @@ let createObj = (obj, id) => {
     let text = document.createTextNode(showInPage.name);
     name.appendChild(text);
     //вывод информации о товаре из db
+    // тут можно и через цикл , для начала создать массив их свойств и крутануть...
     document.querySelector('.product-name').innerText = obj[id].name;
     document.querySelector('.product-country').innerText = obj[id].country;
     document.querySelector('.product-year').innerText = obj[id].year_of_issue;
     document.querySelector('.product-blade').innerText = obj[id].blade;
     document.querySelector('.product-description').innerText = obj[id].description;
     document.querySelector('.product-price').innerText = obj[id].priceForOne;
-
-
-
-    console.log(IMG, name);
 }
-document.body.addEventListener('myNewEvent',updateCounter)
-setInterval(throwMyEvent,200)
+
+
+// хмм, времени мало , так что я запушу в массив , вместо POST в db, протите мою лень ... и да , я никогда не делал POST///
+function PopUpShow(){
+
+}
+document.body.addEventListener('myNewEvent', updateCounter)
+setInterval(throwMyEvent, 200)
